887bcedaac4a39393aef790a7ace14c04228b440
Alex Haynes <alex@alexhaynes.org>
2025-08-19 19:39:44 -0400

    fix build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/windows-98-icons/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
